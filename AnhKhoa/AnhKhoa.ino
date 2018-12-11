6add627673954d87eb13ef19fc470b46bf5dc73c
mmmnic <mmmnic1@gmail.com>
2018-12-11 23:30:40 +0700

    Unknow data won't send to server without delay

--- AnhKhoa/AnhKhoa.ino
+++ AnhKhoa/AnhKhoa.ino
@@ -1,28 +1,20 @@
-#include <SerialCommand.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 
 // Enter your wifi name and password here
-  const char* ssid = "UIT Public";
-  const char* password = "";
+const char* ssid = "            ";
+const char* password = "khongphaicaido";
 
-// Enter you websocket server host and port here 
-  char host[] = "10.45.95.227";  
-  int port = 8000;
+// Enter you websocket server host and port here
+char host[] = "192.168.1.9";
+int port = 8000;
 
 // Create WebSocket client
 WiFiClient client;
 
-// Create Serial to communicate with NUC
-  const byte RX = 3; 
-  const byte TX = 1;
 
-
- SoftwareSerial mySerial = SoftwareSerial(RX, TX, false, 256);
- SerialCommand sCmd(mySerial);
-
- //------------------------ SETUP ---------------------------
+//------------------------ SETUP ---------------------------
 void setup()
 {
   // put your setup code here, to run once:
@@ -30,8 +22,7 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
 
   // create serial to communicate with IDE for debug
-  Serial.begin(9600); 
-  mySerial.begin(9600);
+  Serial.begin(9600);
   delay(50);
 
   //----------------- CONNECT TO WIFI ----------------------
@@ -47,45 +38,43 @@ void setup()
     Serial.println("Connecting..");
   }
 
-  // Print esp8266's ip
+  // Print esp8266's IP
   Serial.print("WiFi connected with IP: ");
   Serial.println(WiFi.localIP());
 
   //----------------- CONNECT TO WEBSOCKET ----------------------
   // Conect to webSocket server also check if fail
   while (!client.connect(host, port)) {
-        Serial.println(F("Ket noi den socket server that bai!"));
-  }
- 
-  // If connection's successed
-  if (client.connected()) {
-      // Print on IDE
-      Serial.println("Connected to Host");
-      // Send esp8266's ip to webSocket server
-      client.print(WiFi.localIP());
-      client.println(" just connected");
+    Serial.println(F("Ket noi den socket server that bai!"));
   }
 
-  // Catch signal from NUC
-  sCmd.addDefaultHandler(SendToServer);
-  Serial.println("Da san sang nhan lenh");
+  // If connection's successed
+  if (client.connected()) {
+    Serial.println("Connected to Host");
+    // Send esp8266's ip to webSocket server
+    client.print(WiFi.localIP());
+    client.println(" just connected");
+  }
 }
 
 void loop()
 {
-  String data ="{\"ID\":\"3\",\"BUTTON\":\"LEFT\"}";
-  SendToServer(data);
-  delay(500);
-  
+  //Receive from server and send to NUC
+  ServerToNuc();
+  //Reveive from NUC and send to Server
+  NucToServer();
 
+  // Nani Delay???
+  delay(100);
 
+ 
   // if the server's disconnected
   if (!client.connected()) {
     Serial.println();
     Serial.println("Lost connection from server!");
     Serial.println("Reconnecting...");
     // Reconnect to WebSocket server
-    while(!client.connect(host, port))
+    while (!client.connect(host, port))
     {
       Serial.println("Reconnecting...");
     }
@@ -98,18 +87,33 @@ void loop()
   }
 }
 
-void SendToServer(String nucData)
-{
-      // Declare Json buffer size = 1024 MB
-      StaticJsonBuffer<1023> jsonBuffer;
 
-      // Decode the data string
-      JsonObject& root = jsonBuffer.parseObject(nucData);
-      // Redecode if decode fail
-      while(!root.success()) {
-      Serial.println("Decode data from NUC failed");
-      JsonObject& root = jsonBuffer.parseObject(nucData);
-      }
+// Decode string then encode to json object and send to Server
+void NucToServer()
+{
+  String dataRecieve = "";
+
+  // Convert byte received into string
+  while (Serial.available()>0) {
+    char c = Serial.read();
+    if (c=='*')
+    {
+      break;
+    }
+    dataRecieve += c;
+  }
+
+  // If string is not empty send to server
+  if (dataRecieve!="")
+  {
+    // Declare Json buffer size = 1024 MB
+    StaticJsonBuffer<1023> jsonBuffer;
+
+    // Decode the data string
+    JsonObject& root = jsonBuffer.parseObject(dataRecieve);
+    // Redecode if decode fail
+    if (root.success())
+    {
       const char* nuc_ID = root["ID"];
       const char* nuc_BUTTON = root["BUTTON"];
 
@@ -119,11 +123,48 @@ void SendToServer(String nucData)
       data["ID"] = nuc_ID;
       data["BUTTON"] = nuc_BUTTON;
 
-     // Send to server
-     client.print("\r\n");
-     data.printTo(client);
-     
-     // Generate the JSON string and print in IDE to debug
-     Serial.println();
-     data.printTo(Serial);
+      // Send to selerver
+      data.printTo(client);
+    }
+  }
+}
+
+void ServerToNuc()
+{
+  String dataRecieve = "";
+  // Convert byte received into string
+  while (client.available()>0) {
+    char c = client.read();
+    if (c=='*')
+    {
+      break;
+    }
+    dataRecieve += c;
+  }
+
+  // If string is not empty send to server
+  if (dataRecieve!="")
+  {
+    // Declare Json buffer size = 1024 MB
+    StaticJsonBuffer<1023> jsonBuffer;
+
+    // Decode the data string
+    JsonObject& root = jsonBuffer.parseObject(dataRecieve);
+    // Redecode if decode fail
+    if (root.success())
+    {
+      const char* nuc_ID = root["ID"];
+      const char* nuc_BUTTON = root["BUTTON"];
+
+      // Create json object
+      JsonObject& data = jsonBuffer.createObject();
+      // assign data to json object
+      data["ID"] = nuc_ID;
+      data["BUTTON"] = nuc_BUTTON;
+
+      // Send to selerver
+      data.printTo(Serial);
+      Serial.print('*');
+    }
+  }
 }
